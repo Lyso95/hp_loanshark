01f69429f4f7c3a4f043b404d85ce98e6d4a4d9e
Lyso95 <amos@langensiepen.net>
2021-11-28 13:40:51 +0100

    pause

--- js/site.js
+++ js/site.js
@@ -1,4 +1,24 @@
-//Call Hello World
-function helloWorld(){
-    alert("Hello World");
+// controller
+
+function getValues(){
+    let kMenge = document.getElementById("kMenge").value;
+    let mRaten = document.getElementById("mRaten").value;
+    let kRate = document.getElementById("kRate").value;
+
+    kreAmmount = parseInt(kMenge);
+    monRate = parseInt(mRaten);
+    kreRate = parseInt(kRate);
+
+    if(Number.isInteger(kreAmmount) && Number.isInteger(monRate) && Number.isInteger(kRate)) {
+        //call Calculate
+        let kreData = calculate(kreAmmount, monRate, kreRate);
+        //call display
+        displayData(kreData);
+    } else {
+        alert("Bitte gib hier nur Zahlen ein!");
+    }
+}
+
+function calculate(kreAmmount, monRate, kreRate){
+    let p
 }
