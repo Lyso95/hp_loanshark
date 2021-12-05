86c2be83d30b9e21ce757a528c450cdf934bc251
Lyso95 <amos@langensiepen.net>
2021-12-05 22:15:21 +0100

    plan

--- js/site.js
+++ js/site.js
@@ -1,17 +1,19 @@
 // controller
 
 function getValues(){
-    let kMenge = document.getElementById("kMenge").value;
+    //get values from page
+    let kBetrag = document.getElementById("kBetrag").value;
     let mRaten = document.getElementById("mRaten").value;
-    let kRate = document.getElementById("kRate").value;
+    let zSatz = document.getElementById("kRate").value;
 
-    kreAmmount = parseInt(kMenge);
-    monRate = parseInt(mRaten);
-    kreRate = parseInt(kRate);
-
-    if(Number.isInteger(kreAmmount) && Number.isInteger(monRate) && Number.isInteger(kRate)) {
+    //parse and call function if valid
+    kredit = parseInt(kBetrag);
+    monat = parseInt(mRaten);
+    zins = parseInt(zSatz);
+    //ist
+    if(Number.isInteger(kredit) && Number.isInteger(monat) && Number.isInteger(zins)) {
         //call Calculate
-        let kreData = calculate(kreAmmount, monRate, kreRate);
+        let kreData = calculate(kredit, monat, zins);
         //call display
         displayData(kreData);
     } else {
@@ -19,6 +21,18 @@ function getValues(){
     }
 }
 
-function calculate(kreAmmount, monRate, kreRate){
-    let p
+//object erstellen
+//monatliche Rate berechnen
+//monatliche Zinsen berechnen
+//Gesamtgebühren pro Monat berechnen
+//Schuldenstand berechnen
+//Monatszins = 100 * ( 12√ 1 + Jahreszins / 100 - 1)
+//Jahreszins = 100 * [ ( 1 + Monatszins / 100 )12 - 1 ]
+function calculate(){
+        //Math.pow(index, potenz) 2, 2 = 4
 }
+
+// --- TODO: Ausgabe ---
+//results = Table
+//kSum => kBetrag (Kreditbetrag)
+//gAufwand = Gesamtaufwand
